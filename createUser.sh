6338823e0b8546a4d676f5cab9c2709bab9b2138
hillipete <83722763+hillipete@users.noreply.github.com>
2021-05-26 16:00:34 -0700

    Update createUser.sh

--- createUser.sh
+++ createUser.sh
@@ -12,20 +12,20 @@ if [ $# -eq 0 ]  #check for user argument and prompt if not found
     echo Please input desired user name.
     read user
     echo Creating user account and setting permissions for user: $user
-    useradd -g sftponly -e $expire $user
-    chown -R root /home/sftponly/$user/
-    chmod -R 700 /home/sftponly/$user/
-    mkdir /home/sftponly/$user/dropbox
-    chown $user /home/sftponly/$user/dropbox/
+    useradd -g xxxx -e $expire $user
+    chown -R root /home/xxxxx/$user/
+    chmod -R 700 /home/xxxx/$user/
+    mkdir /home/xxxx/$user/dropbox
+    chown $user /home/xxxx/$user/dropbox/
 
     echo $password |passwd $user --stdin
 else
     echo Creating user account and setting permissions for user: $1
-    useradd -g sftponly -e $expire $1
-    chown -R root /home/sftponly/$1/
-    chmod -R 700 /home/sftponly/$1/
-    mkdir /home/sftponly/$1/dropbox
-    chown $1 /home/sftponly/$1/dropbox/
+    useradd -g xxxx -e $expire $1
+    chown -R root /home/xxxx/$1/
+    chmod -R 700 /home/xxx/$1/
+    mkdir /home/xxxx/$1/dropbox
+    chown $1 /home/xxxxx/$1/dropbox/
     echo $password |passwd $1 --stdin
 fi
 
